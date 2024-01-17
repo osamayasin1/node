f9acccb43b2c9bbbbab6856bf8fb9e3bf0fa4cc8
Osama Yasin <usama.yaseen44@gmail.com>
2024-01-17 12:47:35 +0500

    node code .

---
+++ hello.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Node.js application behind a reverse proxy!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
